91b4d114e0bcc2799a565fe72318e6d377191fdd
lkhoydal <lkhoydal@gmail.com>
2017-10-24 17:52:43 +0200

    Estetikk

--- problem8.cpp
+++ problem8.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main()
 {
+	// Det _enorme_ 1000-sifrede tallet
 	string tallet = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
 	
 	unsigned int i = 0; // inkrement i loop
@@ -15,27 +16,27 @@ int main()
 	unsigned long long maksProdukt = 0; // initialiserer
 	const int REKKE = 12; // hvor mange tall fremover i tillegg til i som skal regnes ut
 	
-	while((i + REKKE) != tallet.length())
+	while((i + REKKE) != tallet.length()) // rykker frem 1 startposisjon om gangen, til enden av string er nådd
 	{
-		unsigned long long tempProdukt = 0;
+		unsigned long long tempProdukt = 0; // siden det er ukjent lengde, benyttes ullong
 		cout << "Kalkulerer tall: ";
-		for(int j = i; j <= (i + REKKE); j++)
+		for(int j = i; j <= (i + REKKE); j++) // regner fra posisjon "i" i while-loop og 12 steg fremover (13 totalt)
 		{
-			int siffer = tallet[j] - '0';
+			int siffer = tallet[j] - '0'; // "konverterer" fra char til korrekt int-verdi
 			
-			if(0 == tempProdukt)
+			if(0 == tempProdukt) // om første tall i tallrekke, sett som utgangspunkt for produktregning
 				tempProdukt = siffer;
-			else
+			else // ellers gang tempProdukt med nytt siffer i tallrekken
 				tempProdukt *= siffer;
 			
-			cout << siffer;
+			cout << siffer; // lister inneværende siffer til terminal
 		}
 		cout << "\nTempProdukt: " << tempProdukt << endl;
 		
 		if(tempProdukt > maksProdukt)
 			maksProdukt = tempProdukt;
 		
-		tempProdukt = 0;
+		tempProdukt = 0; // resetter tempProdukt til ny iterasjon
 		i++;
 	}
 	
