03c82a3c73f00896339060d31c6b0f584024a133
Lars Kristian Høydal <lkhoydal@gmail.com>
2017-10-24 07:03:34 +0200

    Refaktorisert og endret
    Endret til 1 stk palindrome, ikke vector

--- problem4.cpp
+++ problem4.cpp
@@ -8,7 +8,7 @@ int main()
 {
 	cout << "Høyeste palindrome av 3-sifrede tall" << endl;
 	
-	vector<int> palindrome;
+	int palindrome = 0;
 	int produkt = 0;
 	for(int i = 999; i > 99; i--)
 	{
@@ -19,20 +19,13 @@ int main()
 			string produktet = to_string(static_cast<int>(produkt));
 			if((produktet[0] == produktet[5]) && (produktet[1] == produktet[4]) && (produktet[2] == produktet[3]))
 			{
-				palindrome.push_back(produkt);
+				palindrome = produkt;
+				break;
 			}
+			if(palindrome > 0)
+				break;
 		}
 	}
 	
-	int maxPalindrome = 0;
-	cout << "Mulige palindromer: " << endl;
-	for(int i = 0; i < palindrome.size(); i++)
-	{
-		if(palindrome[i] > maxPalindrome)
-			maxPalindrome = palindrome[i];
-		
-		cout << "Mulig palindrome: " << palindrome[i] << endl;
-	}
-
-	cout << "\n\nDen høyeste palindromen er: " << maxPalindrome << endl;
+	cout << "\n\nHøyeste palindrome er: " << palindrome << endl;
 }
