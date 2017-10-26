ccd22ebe0debcaf9f876c09d041a764b694910de
Lars Kristian Høydal <lkhoydal@gmail.com>
2017-10-26 22:40:38 +0200

    Potensielt fungerende versjon

--- problem12.c
+++ problem12.c
@@ -3,26 +3,41 @@
 
 int main()
 {
-  int triangelTall = 0;
+  unsigned long long triangelTall = 0;
+	int maksDeling = 0;
 
   clock_t start_s = clock();
 
   // WHILE antallGangerDelt < 501
   // FOR j = triangeltall og antallGangerDelt++
-
-  for(int i = 1; i <= 500000; i++)
+	int antallGangerDelt = 0; // resetter for hver iterasjon
+	int i = 1;
+  while(antallGangerDelt < 501)
   {
-    int antallGangerDelt = 0; // resetter for hver iterasjon
+		antallGangerDelt = 0;
     triangelTall += i; // Legger på triangelTall
 
     // Sjekker delbare tall for triangelTall
-    for(int j = triangelTall; j > 0; j--)
-    {
-
-    }
+		for(int j = triangelTall; j > 0; j--)
+		{
+			if(triangelTall % j == 0)
+			{
+				antallGangerDelt++;
+			}
+		}
+		if(antallGangerDelt > maksDeling)
+		{
+			clock_t temp_s = clock();
+			double tempTid = (double)(temp_s - start_s)/CLOCKS_PER_SEC;
+			maksDeling = antallGangerDelt;
+			printf("Runtime (sec): %f // Høyeste delingstall så langt: %i // %llu\n", tempTid, maksDeling, triangelTall);
+		}
+		i++;
   }
+	
+	printf("Triangeltall med over 500 dividenter er: %llu\n", triangelTall);
 
   clock_t stop_s = clock();
   double tid = (double)(stop_s - start_s)/CLOCKS_PER_SEC;
-  printf("\nRuntime (sec): %f\n", tid);
+  printf("\nTotal runtime (sec): %f\n", tid);
 }
